68acb0cbf2b6fe7eecc0133e950da4f8a43a0867
stelmakhivan <stelmakh.developer@gmail.com>
2020-09-04 21:34:54 +0300

    Apollo initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { MikroORM } from '@mikro-orm/core'
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
 
 // import { __prod__ } from './constants'
 // import { Post } from './entities/Post'
+import { HelloResolver } from './resolvers/hello'
 import microConfig from './mikro-orm.config'
 
 const main = async () => {
@@ -10,10 +13,14 @@ const main = async () => {
   await orm.getMigrator().up()
 
   const app = express()
-
-  app.get('/', (_, res) => {
-    res.send('Hello')
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
   })
+
+  apolloServer.applyMiddleware({ app })
 
   app.listen(4000, () => {
     console.log('server started on localhost:4000')

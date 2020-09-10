73c45ca902c58065b46ca5ee328474816d74f99c
stelmakhivan <stelmakh.developer@gmail.com>
2020-09-10 20:28:04 +0300

    Post ui refactoring

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -9,6 +9,8 @@ import {
   Ctx,
   UseMiddleware,
   Int,
+  FieldResolver,
+  Root,
 } from 'type-graphql'
 
 import { Post } from '../entities/Post'
@@ -24,8 +26,13 @@ class PostInput {
   text: string
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50)
+  }
+
   @Query(() => [Post])
   async posts(
     @Arg('limit', () => Int /* , { defaultValue: 10 } */) limit: number,
